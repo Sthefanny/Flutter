e1b8ed6c31302720fba003115362620f2237bf39
Sthefanny <s.gonzaga@cinq.com.br>
2018-12-29 21:53:13 -0200

    beginning of login_stateful classes

---
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text('Submit!'),
+      onPressed: () {
+        print("Pressed!");
+      },
+    );
+  }
+}
