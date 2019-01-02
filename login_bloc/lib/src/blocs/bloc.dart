308dbbde18a0f5072feeccbb548477fdae748861
Sthefanny <s.gonzaga@cinq.com.br>
2019-01-02 08:17:07 -0200

    beginning of login_bloc

---
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'validators.dart';
+
+// class Bloc extends Object with Validators {
+class Bloc with Validators {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  // Add data to stream
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  // Change data
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
+}
+
+final bloc = Bloc();
---
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login!'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
